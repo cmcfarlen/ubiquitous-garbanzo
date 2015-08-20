0627df8acf304c3a20833b6a8df18cf960edc2eb
Chris McFarlen <mcfarlen@yahoo-inc.com>
2015-08-20 10:10:09 -0500

    get login info from the environment to deploy code.

--- scripts/build.clj
+++ scripts/build.clj
@@ -5,16 +5,19 @@
 
 (defn deploy-build
   []
-  (println "deploying code...")
-  (client/post "https://screeps.com/api/user/code"
-               {:basic-auth ["email" "password"]
-                :content-type :json
-                :form-params {:branch "test"
-                              :modules {:main (slurp "release/wtf.js")}}
-                }
-               )
-  (println "done!")
-  )
+  (let [user (System/getenv "SCREEPS_USERNAME")
+        passwd (System/getenv "SCREEPS_PASSWORD")
+        branch (or (System/getenv "SCREEPS_BRANCH") "test")]
+    (if (and user passwd)
+      (do
+       (println "deploying code...")
+       (client/post "https://screeps.com/api/user/code"
+                    {:basic-auth [user passwd]
+                     :content-type :json
+                     :form-params {:branch branch
+                                   :modules {:main (slurp "release/wtf.js")}}})
+       (println "done!"))
+      (println "export SCREEPS_USERNAME and SCREEPS_PASSWORD (optionally SCREEPS_BRANCH) to autodeploy code."))))
 
 (let [start (System/nanoTime)]
   (b/watch "src"
