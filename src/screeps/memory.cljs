89f8482624e7ea45786d8ac178e8208a9228b02f
Chris McFarlen <mcfarlen@yahoo-inc.com>
2015-08-24 21:10:53 -0500

    updates to screeps.memory.

--- src/screeps/memory.cljs
+++ src/screeps/memory.cljs
@@ -1,19 +1,22 @@
 (ns screeps.memory)
 
-(defn memory
+(defn fetch
   ([]
    (js->clj js/Memory))
   ([k]
-   (js->clj (aget js/Memory (name k)) :keywordize-keys true)))
+   (fetch k nil))
+  ([k default]
+   (if-let [m (aget js/Memory (name k))]
+     (js->clj m :keywordize-keys true)
+     default)))
 
-(defn store
+(defn store!
   [k o]
   (aset js/Memory (name k) (clj->js o)))
 
-(defn update
+(defn update!
   "call f with memory location k and store the result back in k"
-  [k f]
-  (let [d (memory k)]
-    (store k (f d))))
-
+  [k f & args]
+  (let [d (fetch k)]
+    (store k (apply f d args))))
 
